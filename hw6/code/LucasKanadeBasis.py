6c5c9453feb0dd3f6d5a39c41277c9d97d7e6784
ouyangjiahong <ouyangjiahong13@gmail.com>
2018-12-08 08:53:41 -0500

    finish section 2 with some bugs

--- hw6/code/LucasKanadeBasis.py
+++ hw6/code/LucasKanadeBasis.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pdb
 from scipy.interpolate import RectBivariateSpline
 
 def LucasKanadeBasis(It, It1, rect, bases):
-	# Input: 
+	# Input:
 	#	It: template image
 	#	It1: Current image
 	#	rect: Current position of the car
@@ -12,5 +13,58 @@ def LucasKanadeBasis(It, It1, rect, bases):
 	#	p: movement vector [dp_x, dp_y]
 
     # Put your implementation here
-    return np.zeros(2)
-    
+    p1 = np.zeros(2)
+    thres = 0.01
+    height, width = It.shape
+    It = RectBivariateSpline(np.arange(0, height), np.arange(0, width), It)
+    It1 = RectBivariateSpline(np.arange(0, height), np.arange(0, width), It1)
+
+    x1, y1, x2, y2 = rect
+    rect_h = y2 - y1 + 1
+    rect_w = x2 - x1 + 1
+    Y, X = np.meshgrid(np.arange(y1,y1+rect_h), np.arange(x1,x1+rect_w))
+    it = It.ev(X, Y)
+
+    B = np.reshape(bases, (bases.shape[0]*bases.shape[1], bases.shape[2]))
+    Gx, Gy = np.gradient(it)
+    G = np.stack([Gx.flatten(), Gy.flatten()], axis=1)
+    dwdp = np.eye(2)
+    A = np.dot(G, dwdp)
+    HA = A - np.dot(np.dot(B, B.T), A)
+    H = np.dot(HA.T, HA)
+
+    while True:
+        it1 = It1.ev(X+p1[0], Y+p1[1])
+
+        # compute error
+        error = it - it1
+        # pdb.set_trace()
+
+        # compute gradient
+        # Gx, Gy = np.gradient(it1)
+        # G = np.stack([Gx.flatten(), Gy.flatten()], axis=1)
+        #
+        # # #Jacobian
+        # dwdp = np.eye(2)
+        #
+        # # Hessian
+        # A = np.dot(G, dwdp)
+        # HA = A - np.dot(np.dot(B, B.T), A)
+        # H = np.dot(HA.T, HA)
+
+        # dp
+        b = error.flatten()
+        Hb = b - np.dot(np.dot(B, B.T), b)
+        dp = np.dot(np.linalg.inv(H), np.dot(HA.T, Hb))
+
+        # update
+        p1 = p1 - dp
+
+        # check whether smaller than thres
+        if np.linalg.norm(dp) < thres:
+            break
+
+        print(np.linalg.norm(dp))
+        # print(p1)
+
+    return p1
--- hw6/code/testSylvSequence.py
+++ hw6/code/testSylvSequence.py
@@ -1,6 +1,63 @@
 import numpy as np
+import pdb
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import matplotlib.patches as patches
+from LucasKanadeBasis import *
+from LucasKanade import *
 
 # write your script here, we recommend the above libraries for making your animation
+video_path = '../data/sylvseq.npy'
+base_path = '../data/sylvbases.npy'
+init_rect = [61.0, 101.0, 107.0, 155.0]
+frame_step = 1
+
+data = np.load(video_path)
+frame_num = data.shape[2]
+bases = np.load(base_path)
+
+rect_list = [init_rect]
+for i in np.arange(1, frame_num, frame_step):
+    # pdb.set_trace()
+    img_pre = data[:,:,i-1]
+    img_cur = data[:,:,i]
+    p1 = LucasKanadeBasis(img_pre, img_cur, init_rect, bases)
+    init_rect = init_rect + np.array([p1[0], p1[1], p1[0], p1[1]])
+    rect_list.append(init_rect)
+    print(p1)
+
+init_rect = [61.0, 101.0, 107.0, 155.0]
+rect_list_old = [init_rect]
+for i in np.arange(1, frame_num, frame_step):
+    # pdb.set_trace()
+    img_pre = data[:,:,i-1]
+    img_cur = data[:,:,i]
+    p1 = LucasKanade(img_pre, img_cur, init_rect)
+    init_rect = init_rect + np.array([p1[0], p1[1], p1[0], p1[1]])
+    rect_list_old.append(init_rect)
+    print(p1)
+
+# save rects
+rects = np.array(rect_list)[:,[1,0,3,2]]
+np.save('sylvseqrects.npy', rects)
+rects_old = np.array(rect_list_old)[:,[1,0,3,2]]
+np.save('sylvseqrects-old.npy', rects_old)
+
+# visualization
+frame = [0, 100, 200, 300, 400]
+fig, axs = plt.subplots(1, 5, figsize=(10,2))
+for i, k in enumerate(frame):
+    img = axs[i].imshow(data[:,:,k], cmap='gray')
+    rect = rect_list_old[k]
+    r = patches.Rectangle((rect[1], rect[0]),
+                           round(rect[3] - rect[1] + 1),
+                           round(rect[2] - rect[0] + 1),
+                           fill=False, edgecolor='r')
+    axs[i].add_patch(r)
+    rect = rect_list[k]
+    r = patches.Rectangle((rect[1], rect[0]),
+                           round(rect[3] - rect[1] + 1),
+                           round(rect[2] - rect[0] + 1),
+                           fill=False, edgecolor='g')
+    axs[i].add_patch(r)
+plt.show()
